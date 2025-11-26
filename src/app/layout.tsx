4fc3ecea1c166449ccf755a480fd95fae72f6455
Ashish garg <ashish47705@gmail.com>
2025-11-26 12:34:49 -0500

    layout error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {Navbar}  from "@/components/Navbar";
 
-const inter = Inter({weight: ["400","500","600","700","800","900"]});
+const inter = Inter({weight: ["400","500","600","700","800","900"],
+  preload: false,
+}
+);
 
 
 
